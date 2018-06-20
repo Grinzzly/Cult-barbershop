b658e21671f7e70a8645f9d4de0acc51ea736c7b
Sergei Grin <grinzzly@gmail.com>
2018-06-20 10:36:22 +0300

    Additional facades implementation

--- js/function.js
+++ js/function.js
@@ -1,60 +1,58 @@
-(function ($) {
+const $window = $(window);
 
-	const $window = $(window);
+/* Preloader Effect */
+$window.load(function () {
+  $(".preloader").fadeOut(400);
+});
 
-	/* Preloader Effect */
-	$window.load(function() {
-	    $(".preloader").fadeOut(600);
-    });
+/* Top Menu */
+$(".navbar-link-item").on('click', function () {
+  console.log(1);
+  const id = $(this).attr('href');
+  const h = parseFloat($(id).offset().top);
+  $('body,html').stop().animate({
+    scrollTop: h - 70
+  }, 800);
 
-	/* Top Menu */
-	$('#navigation ul li a').on('click', function(){
-		const id = $(this).attr('href');
-		const h = parseFloat($(id).offset().top);
-		$('body,html').stop().animate({
-			scrollTop: h - 70
-		}, 800);
-		$(".navbar-collapse").collapse("hide");
+  $(".navbar-collapse").collapse("hide");
 
-		return false;
-	});
+  return false;
+});
 
-	/* Sticky header */
-	$window.scroll(function(){
-    	if ($window.scrollTop() > 200) {
-			$('.navbar').addClass('sticky-header');
-		} else {
-			$('.navbar').removeClass('sticky-header');
-		}
-	});
+/* Sticky header */
+$window.scroll(function () {
+  if ($window.scrollTop() > 200) {
+    $('.navbar').addClass('sticky-header');
+  } else {
+    $('.navbar').removeClass('sticky-header');
+  }
+});
 
-	/* slick nav */
-	$('#main-menu').slicknav({prependTo:'#responsive-menu',label:''});
+/* slick nav */
+$('#main-menu').slicknav({prependTo: '#responsive-menu', label: ''});
 
-	/*OwlCarousels Testimonial Start*/
-	$('#testimonial-slider').owlCarousel({
-		loop: true,
-		items: 3,
-		margin: 30,
-		responsiveClass: true,
-		responsive : {
-			0 : {
-				items: 1
-			},
+/*OwlCarousels Testimonial Start*/
+$('#testimonial-slider').owlCarousel({
+  loop: true,
+  items: 3,
+  margin: 30,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1
+    },
 
-			768 : {
-				items: 2
-			},
+    768: {
+      items: 2
+    },
 
-			1024 : {
-				items: 3
-			}
-		},
-		nav: true,
-		dots: false,
-		autoplay: true,
-		autoplaySpeed: 1000,
-		navText: [ '<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>'],
-	});
-
-})(jQuery);
+    1024: {
+      items: 3
+    }
+  },
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+});
